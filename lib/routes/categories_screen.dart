16a0401fdddfbdc07cab9ba84dfa554f7e2cfb53
Luis Felipe Bertel Mercado <luis.bertel@gorillalogic.com>
2020-10-10 21:33:45 -0500

    Initial

---
+++ lib/routes/categories_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../seeds/dummy_data.dart';
+import '../widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'DeliMeal',
+        ),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map((item) => CategoryItem(
+                  title: item.title,
+                  color: item.color,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
