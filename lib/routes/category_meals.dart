df677393811a3b11bf53e6cb85239805c66fcfa6
Luis Felipe Bertel <ellipe@gmail.com>
2020-10-12 10:57:39 -0500

    added models and image network.

--- lib/routes/category_meals.dart
+++ lib/routes/category_meals.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
+import '../seeds/dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    final routeArg = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final routeArg =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArg['title'];
-    
+    final categoryId = routeArg['id'];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
           categoryTitle,
         ),
       ),
-      body: Center(
-        child: Text('This is a category'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            affordability: categoryMeals[index].affordability,
+            duration: categoryMeals[index].duration,
+            complexity: categoryMeals[index].complexity,
+          );
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
